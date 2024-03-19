ace3e4857ef0222959e477e9e8ef3f90448643c9
Will Patterson <willpatterson@Wills-MacBook-Pro.local>
2024-03-19 16:58:26 -0700

    Adding terraform files and folder

---
+++ instavote/terraform/0-provider.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+
+  region = "us-east-1"
+  
+
+}
+
+variable "cluster_name" {
+  default = "ist"
+}
+
+variable "cluster_version" {
+  default = "1.25"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+  }
+}
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
